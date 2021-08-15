6d45a444715b5ca97a237c53f50bba1eb6c3279d
Edmund Rotimi <support@scholarometer.com>
2021-08-15 23:33:16 +0100

    remove redirect middleware

--- tem/settings.py
+++ tem/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 # Application definition
 
@@ -78,8 +78,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # django admin bookmarklets
     'django.contrib.admindocs.middleware.XViewMiddleware',
-    # current user
-    'tem.middleware.remoteAddr.RemoteAddrMiddleware',
+    # redirect middleware
+    #'tem.middleware.remoteAddr.RemoteAddrMiddleware',
     # current user
     'tem.middleware.currentUser.RequestMiddleware',
 
